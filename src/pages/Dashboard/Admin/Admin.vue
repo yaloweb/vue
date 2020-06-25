9d6eb67ae70c80d940da2d66374e72bf19eb0c0b
Ravil <whiteravil@yandex.ru>
2020-06-25 18:06:43 +0300

    nothing

--- src/pages/Dashboard/Admin/Admin.vue
+++ src/pages/Dashboard/Admin/Admin.vue
@@ -44,7 +44,7 @@
                     </div>
                     <div class="form-model-item-btns">
                         <l-button type="primary" @click="editField(stepItem)">Редактировать</l-button>
-                        <l-button type="danger">Удалить</l-button>
+                        <l-button type="danger" @click="deleteField(stepItem, step.name)">Удалить</l-button>
                     </div>
                 </div>
                 <div class="text-right pt-3">
@@ -52,6 +52,10 @@
                               @click="addNewField(step.name)">Добавить новое поле</l-button>
                 </div>
             </card>
+        </card>
+
+        <card title="Гистограмма" subTitle="Статистика заявок">
+            <div id="chartViews" class="ct-chart"></div>
         </card>
 
         <el-dialog :visible.sync="modals.stepModal"
@@ -393,6 +397,331 @@
         editFieldItem: {},
         modelTypes: ['input', 'textarea', 'select', 'checkbox', 'radio'],
         fieldTypes: ['text', 'email', 'date'],
+        tableData: [
+          {
+            "id": 1,
+            "name": "Лапенко Игорь Николаевич",
+            "phone": "+7 919 919-99-99",
+            "status": 1,
+            "price": 40000,
+            "city": "Москва",
+            "birthDay": "25/05/1979",
+            "religion": "Христианство",
+            "statusName": "Новая"
+          },
+          {
+            "id": 2,
+            "name": "Бодрова Наталья Владимировна",
+            "phone": "+7 919 919-99-99",
+            "status": 2,
+            "price": 150000,
+            "city": "Казань",
+            "birthDay": "15/12/1968",
+            "religion": "Иудаизм",
+            "statusName": "Анкета заполнена"
+          },
+          {
+            "id": 3,
+            "name": "Кулич Евгений Баженович",
+            "phone": "+7 919 919-99-99",
+            "status": 3,
+            "price": 50000,
+            "city": "Москва",
+            "birthDay": "01/10/1982",
+            "religion": "Христианство",
+            "statusName": "Анкета подтверждена"
+          },
+          {
+            "id": 4,
+            "name": "Салахова Кристина Андреевна",
+            "phone": "+7 919 919-99-99",
+            "status": 4,
+            "price": 110000,
+            "city": "Санкт-Петербург",
+            "birthDay": "5/05/1961",
+            "religion": "Ислам",
+            "statusName": "Оформлено"
+          },
+          {
+            "id": 5,
+            "name": "Попов Дмитрий Александрович",
+            "phone": "+7 919 919-99-99",
+            "status": 5,
+            "price": 40000,
+            "city": "Казань",
+            "birthDay": "01/02/1959",
+            "religion": "Буддизм",
+            "statusName": "Отказано"
+          },
+          {
+            "id": 6,
+            "name": "Навальный Алексей Беларусович",
+            "phone": "+7 919 919-99-99",
+            "status": 1,
+            "price": 250000,
+            "city": "Казань",
+            "birthDay": "21/04/1971",
+            "religion": "Индуизм",
+            "statusName": "Новая"
+          },
+          {
+            "id": 7,
+            "name": "Андреев Андрей Александрович",
+            "phone": "+7 919 919-99-99",
+            "status": 2,
+            "price": 180000,
+            "city": "Москва",
+            "birthDay": "01/02/1940",
+            "religion": "Ислам",
+            "statusName": "Анкета заполнена"
+          },
+          {
+            "id": 8,
+            "name": "Карманов Семен Михайлович",
+            "phone": "+7 919 919-99-99",
+            "status": 3,
+            "price": 250000,
+            "city": "Санкт-Петербург",
+            "birthDay": "15/05/1976",
+            "religion": "Индуизм",
+            "statusName": "Анкета подтверждена"
+          },
+          {
+            "id": 9,
+            "name": "Лапенко Игорь Николаевич",
+            "phone": "+7 919 919-99-99",
+            "status": 1,
+            "price": 40000,
+            "city": "Москва",
+            "birthDay": "25/05/1979",
+            "religion": "Христианство",
+            "statusName": "Новая"
+          },
+          {
+            "id": 10,
+            "name": "Бодрова Наталья Владимировна",
+            "phone": "+7 919 919-99-99",
+            "status": 2,
+            "price": 150000,
+            "city": "Казань",
+            "birthDay": "15/12/1968",
+            "religion": "Иудаизм",
+            "statusName": "Анкета заполнена"
+          },
+          {
+            "id": 11,
+            "name": "Кулич Евгений Баженович",
+            "phone": "+7 919 919-99-99",
+            "status": 3,
+            "price": 50000,
+            "city": "Москва",
+            "birthDay": "01/10/1982",
+            "religion": "Христианство",
+            "statusName": "Анкета подтверждена"
+          },
+          {
+            "id": 12,
+            "name": "Салахова Кристина Андреевна",
+            "phone": "+7 919 919-99-99",
+            "status": 4,
+            "price": 110000,
+            "city": "Санкт-Петербург",
+            "birthDay": "5/05/1961",
+            "religion": "Ислам",
+            "statusName": "Оформлено"
+          },
+          {
+            "id": 13,
+            "name": "Попов Дмитрий Александрович",
+            "phone": "+7 919 919-99-99",
+            "status": 5,
+            "price": 80000,
+            "city": "Казань",
+            "birthDay": "01/02/1959",
+            "religion": "Буддизм",
+            "statusName": "Отказано"
+          },
+          {
+            "id": 14,
+            "name": "Навальный Алексей Беларусович",
+            "phone": "+7 919 919-99-99",
+            "status": 1,
+            "price": 250000,
+            "city": "Казань",
+            "birthDay": "21/04/1971",
+            "religion": "Индуизм",
+            "statusName": "Новая"
+          },
+          {
+            "id": 15,
+            "name": "Андреев Андрей Александрович",
+            "phone": "+7 919 919-99-99",
+            "status": 2,
+            "price": 180000,
+            "city": "Москва",
+            "birthDay": "01/02/1940",
+            "religion": "Ислам",
+            "statusName": "Анкета заполнена"
+          },
+          {
+            "id": 16,
+            "name": "Карманов Семен Михайлович",
+            "phone": "+7 919 919-99-99",
+            "status": 3,
+            "price": 40000,
+            "city": "Санкт-Петербург",
+            "birthDay": "15/05/1976",
+            "religion": "Индуизм",
+            "statusName": "Анкета подтверждена"
+          },
+          {
+            "id": 17,
+            "name": "Лапенко Игорь Николаевич",
+            "phone": "+7 919 919-99-99",
+            "status": 1,
+            "price": 100000,
+            "city": "Москва",
+            "birthDay": "25/05/1979",
+            "religion": "Христианство",
+            "statusName": "Новая"
+          },
+          {
+            "id": 18,
+            "name": "Бодрова Наталья Владимировна",
+            "phone": "+7 919 919-99-99",
+            "status": 2,
+            "price": 150000,
+            "city": "Казань",
+            "birthDay": "15/12/1968",
+            "religion": "Иудаизм",
+            "statusName": "Анкета заполнена"
+          },
+          {
+            "id": 19,
+            "name": "Кулич Евгений Баженович",
+            "phone": "+7 919 919-99-99",
+            "status": 3,
+            "price": 50000,
+            "city": "Москва",
+            "birthDay": "01/10/1982",
+            "religion": "Христианство",
+            "statusName": "Анкета подтверждена"
+          },
+          {
+            "id": 20,
+            "name": "Салахова Кристина Андреевна",
+            "phone": "+7 919 919-99-99",
+            "status": 4,
+            "price": 110000,
+            "city": "Санкт-Петербург",
+            "birthDay": "5/05/1961",
+            "religion": "Ислам",
+            "statusName": "Оформлено"
+          },
+          {
+            "id": 21,
+            "name": "Попов Дмитрий Александрович",
+            "phone": "+7 919 919-99-99",
+            "status": 5,
+            "price": 40000,
+            "city": "Казань",
+            "birthDay": "01/02/1959",
+            "religion": "Буддизм",
+            "statusName": "Отказано"
+          },
+          {
+            "id": 22,
+            "name": "Навальный Алексей Беларусович",
+            "phone": "+7 919 919-99-99",
+            "status": 1,
+            "price": 250000,
+            "city": "Казань",
+            "birthDay": "21/04/1971",
+            "religion": "Индуизм",
+            "statusName": "Новая"
+          },
+          {
+            "id": 23,
+            "name": "Андреев Андрей Александрович",
+            "phone": "+7 919 919-99-99",
+            "status": 2,
+            "price": 180000,
+            "city": "Москва",
+            "birthDay": "01/02/1940",
+            "religion": "Ислам",
+            "statusName": "Анкета заполнена"
+          },
+          {
+            "id": 24,
+            "name": "Карманов Семен Михайлович",
+            "phone": "+7 919 919-99-99",
+            "status": 3,
+            "price": 250000,
+            "city": "Санкт-Петербург",
+            "birthDay": "15/05/1976",
+            "religion": "Индуизм",
+            "statusName": "Анкета подтверждена"
+          },
+          {
+            "id": 25,
+            "name": "Лапенко Игорь Николаевич",
+            "phone": "+7 919 919-99-99",
+            "status": 1,
+            "price": 100000,
+            "city": "Москва",
+            "birthDay": "25/05/1979",
+            "religion": "Христианство",
+            "statusName": "Новая"
+          },
+          {
+            "id": 26,
+            "name": "Бодрова Наталья Владимировна",
+            "phone": "+7 919 919-99-99",
+            "status": 2,
+            "price": 150000,
+            "city": "Казань",
+            "birthDay": "15/12/1968",
+            "religion": "Иудаизм",
+            "statusName": "Анкета заполнена"
+          },
+          {
+            "id": 27,
+            "name": "Кулич Евгений Баженович",
+            "phone": "+7 919 919-99-99",
+            "status": 3,
+            "price": 50000,
+            "city": "Москва",
+            "birthDay": "01/10/1982",
+            "religion": "Христианство",
+            "statusName": "Анкета подтверждена"
+          },
+          {
+            "id": 28,
+            "name": "Салахова Кристина Андреевна",
+            "phone": "+7 919 919-99-99",
+            "status": 4,
+            "price": 110000,
+            "city": "Санкт-Петербург",
+            "birthDay": "5/05/1961",
+            "religion": "Ислам",
+            "statusName": "Оформлено"
+          },
+          {
+            "id": 29,
+            "name": "Попов Дмитрий Александрович",
+            "phone": "+7 919 919-99-99",
+            "status": 5,
+            "price": 80000,
+            "city": "Казань",
+            "birthDay": "01/02/1959",
+            "religion": "Буддизм",
+            "statusName": "Отказано"
+          }
+        ],
+        chartistData: {
+          labels: [],
+          series: [[]]
+        },
       }
     },
     methods: {
@@ -407,7 +736,47 @@
         this.activeField = field.label
         this.modals.editField = true
         this.editFieldItem = field
+      },
+      deleteField(field, stepName) {
+        let deleteConfirm = confirm(`Вы действительно хотите удалить поле ${field.label} из шага '${stepName}'`)
+      },
+      initChart () {
+        const optionsViews = {
+          seriesBarDistance: 10,
+          classNames: {
+            bar: 'ct-bar'
+          },
+          axisX: {
+            showGrid: false
+          },
+          height: '250px'
+        }
+        const responsiveOptionsViews = [
+          ['screen and (max-width: 640px)', {
+            seriesBarDistance: 5,
+            axisX: {
+              labelInterpolationFnc (value) {
+                return value[0]
+              }
+            }
+          }]
+        ]
+        this.$Chartist.Bar('#chartViews', this.chartistData, optionsViews, responsiveOptionsViews)
       }
+    },
+    async mounted() {
+      let chartist = await import('chartist')
+      this.$Chartist = chartist.default ? chartist.default : chartist;
+      let dataPromise = new Promise(resolve => {
+        this.tableData.forEach((item, index) => {
+          this.chartistData.labels.push(index)
+          this.chartistData.series[0].push(item.price)
+          if ( this.tableData.length == index + 1 ) {
+            resolve()
+          }
+        })
+      })
+      dataPromise.then(this.initChart)
     }
   }
 </script>
